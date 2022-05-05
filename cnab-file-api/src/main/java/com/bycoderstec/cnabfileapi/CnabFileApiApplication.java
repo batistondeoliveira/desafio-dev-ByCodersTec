4d747dcab4f4285ac429aa821aab9824595a0bc2
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-05-04 22:15:22 -0300

    feat: add cnab-file-api

---
+++ cnab-file-api/src/main/java/com/bycoderstec/cnabfileapi/CnabFileApiApplication.java
@@ -0,0 +1,13 @@
+package com.bycoderstec.cnabfileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnabFileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnabFileApiApplication.class, args);
+	}
+
+}
---
+++ cnab-file-api/src/test/java/com/bycoderstec/cnabfileapi/CnabReaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bycoderstec.cnabfileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabReaderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
