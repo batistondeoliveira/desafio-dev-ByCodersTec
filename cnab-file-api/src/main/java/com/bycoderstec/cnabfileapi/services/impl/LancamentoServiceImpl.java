d842e2bf8f691b0e40c03b4fa7244025a8646062
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-05-08 14:23:34 -0300

    fix: remove @RequiredArgsConstructor and add @Autowired

--- cnab-file-api/src/main/java/com/bycoderstec/cnabfileapi/services/impl/LancamentoServiceImpl.java
+++ cnab-file-api/src/main/java/com/bycoderstec/cnabfileapi/services/impl/LancamentoServiceImpl.java
@@ -2,18 +2,17 @@ package com.bycoderstec.cnabfileapi.services.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bycoderstec.cnabfileapi.domain.Lancamento;
 import com.bycoderstec.cnabfileapi.repositories.LancamentoRepository;
 import com.bycoderstec.cnabfileapi.services.LancamentoService;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 public class LancamentoServiceImpl implements LancamentoService {
 	
+	@Autowired
 	private LancamentoRepository repository;
 
 	@Override
