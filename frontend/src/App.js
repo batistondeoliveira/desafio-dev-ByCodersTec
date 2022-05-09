955388764056e68e8a2e5b9c15192493bdb94847
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-05-09 14:13:55 -0300

    feat: add route to Cnab component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import {
   BrowserRouter,
-  Routes
+  Routes,
+  Route
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Cnab from "./components/Cnab";
 
 function App() {
   return (
     <BrowserRouter basename="/cnab-frontend">
       <Navbar />
       <Routes>
-        
+        <Route path="/" element={<Cnab />} />        
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Cnab/index.js
@@ -0,0 +1,9 @@
+function Cnab() {
+  return (
+    <>
+    Cnab
+    </>
+  )
+}
+
+export default Cnab;
