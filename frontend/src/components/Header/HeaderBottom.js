cef902cfc4d97ab16ec7a7c3a1e3264d8cebf434
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-05-12 17:06:08 -0300

    refactor: refactor HeaderBottom

--- frontend/src/components/Header/HeaderBottom.js
+++ frontend/src/components/Header/HeaderBottom.js
@@ -1,15 +1,33 @@
-const HeaderBottom = () => 
-  <div className="header-bottom">
-    <nav>
-      <ul>
-        <li>
-          <a href="#">Importar</a>
-        </li>
-        <li>
-          <a href="#">Listar</a>
-        </li>          
-      </ul>
-    </nav>
-  </div>
-  
+import React from 'react';
+import { Redirect, Link } from 'react-router-dom';
+
+const HeaderBottom = () => {
+  const menu = [    
+    {name: "Importar", link: "/"}, 
+    {name: "Relatório", link: "/relatorio"}
+  ];
+
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  return (
+    <div className="header-bottom">
+      <nav>
+        <ul>
+          {menu.map((menu, index) => 
+            <li 
+              key={index} 
+              className={index === activeIndex ? 'active' : ''} 
+              onClick={() => setActiveIndex(index)}
+            >
+              <Link to={menu.link}>
+                {menu.name}
+              </Link>
+            </li>
+          )}                  
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
 export default HeaderBottom;
