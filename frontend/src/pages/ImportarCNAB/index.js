03451b74acff647805f965dc4b2062fba1b170ef
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-05-12 17:07:47 -0300

    refactor: refactor ImpotarCNAB

--- frontend/src/pages/ImportarCNAB/index.js
+++ frontend/src/pages/ImportarCNAB/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import FormCnab from './FormCnab';
-import FormResult from './FormResult';
+import Main from '../../components/Main';
 
 function ImportarCNAB() {
     const [processado, setProcessado] = React.useState(false);
     const [data, setData] = React.useState([]);
 
     return (   
-      <>
-        {!processado
-          ? <FormCnab callback={(data) => {setData(data); setProcessado(true)}} />
-          : <FormResult data={data} callback={() => {setData([]); setProcessado(false)}} />
-        }        
-      </>
+      <Main title="Importar arquivo CNAB">
+        <form className="app-grid-form">
+          <div className="app-grid-form-input">
+            <label>Selecione o arquivo CNAB</label>
+            <input 
+              type="file"               
+              style={{display: 'none'}}
+              accept="application/txt"              
+            />
+
+            <div className="form-control">
+              <button type="button" className="btn btn-danger">Escolher arquivo</button>
+              {/* <span>{file.fileName}</span> */}
+            </div>
+          </div>              
+          <div className="app-grid-form-button">
+            <button type="button" className="btn">Processar</button>
+          </div>
+        </form>
+      </Main>
     )
 }
 
