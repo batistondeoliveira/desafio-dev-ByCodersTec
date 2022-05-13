8803666da8a7a829346ad55af24c410cc453a658
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-05-13 18:51:59 -0300

    feat: loading routes lazy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import {
+import { Suspense, lazy } from "react";
+import {  
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import ImportarCNAB from "./pages/ImportarCNAB";
-import RelatorioFinanceiro from "./pages/RelatorioFinanceiro";
+import Preload from "./components/Preload";
+
+const ImportarCNAB = lazy(() => import("./pages/ImportarCNAB"));
+const RelatorioFinanceiro = lazy(() => import("./pages/RelatorioFinanceiro"));
 
 const App = () =>
-  <BrowserRouter basename="/cnab-frontend">        
-    <Navbar />
-    
-    <Routes>      
-      <Route path="/" element={<ImportarCNAB />} />
-      <Route path="/relatorio" element={<RelatorioFinanceiro />} />
-    </Routes>
-    
-    <Footer />
+  <BrowserRouter basename="/cnab-frontend">    
+    <Suspense fallback={<Preload show={true} />}>
+      <Navbar />
+
+      <Routes>      
+        <Route exact path="/" element={<ImportarCNAB />} />
+        <Route path="/relatorio" element={<RelatorioFinanceiro />} />
+      </Routes>
+
+      <Footer />      
+    </Suspense>    
   </BrowserRouter>            
 
 export default App;
