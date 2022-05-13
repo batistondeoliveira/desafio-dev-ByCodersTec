dd9553cad6d4eb05e41b7b8872641e04c0d46d78
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-05-13 18:36:02 -0300

    feat: add className and ModalError into Main Component

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -1,15 +1,25 @@
 import Preload from '../Preload';
+import { ModalError } from '../Modal';
 
-const Main = ({ title, children, preload }) =>
+const Main = ({ className, title, children, preload, messageError, handleErrorClick }) =>
   <main className="app-grid">
     <Preload show={preload} />
+    <ModalError 
+      open={messageError !== undefined} 
+      message={messageError}
+      handleClose={handleErrorClick} 
+    />
 
     <h2>{title}</h2>
     <div className="app-grid-container">
-      <div className="app-grid-card">  
+      <div className={`app-grid-card ${className}`}>  
          {children}
       </div>            
     </div>
   </main>
+
+Main.defaultProps = {
+  message: ''
+}
 
 export default Main;
